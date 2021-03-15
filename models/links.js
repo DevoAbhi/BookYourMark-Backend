d029be1cb0c8a10217bb84e741ba09267e5a787f
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-15 13:52:38 +0530

    adding type parameter to links model

--- models/links.js
+++ models/links.js
@@ -3,12 +3,15 @@ const Schema = mongoose.Schema;
 
 const linksSchema = new Schema({
     title:{
+        type: String,
         required: true
     },
     href: {
+        type: String,
         required: true
     },
     description: {
+        type: String,
         required: true
     }
 })
