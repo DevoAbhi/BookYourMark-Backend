f27171fcebc10bd7c92451f5f9a2a6ae61e7748b
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-16 03:32:03 +0530

    Add login post route

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const authController = require('../controllers/auth')
 
 router.post('/signup', authController.postSignUp)
+router.post('/login', authController.postLogin)
 
 module.exports = router;
