26424372e5abe58679ac0a1abc805ffddeec5e72
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-15 06:46:27 +0530

    Made some changes in auth.js file

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,14 +7,14 @@ exports.postSignUp = (req, res, next) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
+    console.log(username)
 
-    if(password.toString() != confirmPassword.toString()){
-        return res.status(500).json({
-            success: false,
-            message: "Confirm Password is not same as password"
-        })
-    }
+    // if(password.toString() != confirmPassword.toString()){
+    //     return res.status(500).json({
+    //         success: false,
+    //         message: "Confirm Password is not same as password"
+    //     })
+    // }
 
     User.findOne({ email: email}, (err, user) => {
         if(err){
@@ -35,23 +35,28 @@ exports.postSignUp = (req, res, next) => {
             const user = new User({
                 username: username,
                 email: email,
-                password: password
+                password: hashedPassword
             })
 
             return user.save();
-        }).then((err, result) => {
+        })
+        .then((err, result) => {
             if(err){
                 console.log(err)
                 return res.status(500).json({
                     success: false,
-                    message: err.message
+                    message: "Something went wrong while saving"
                 })
             }
             console.log("User has been registered!");
+            console.log(result);
             res.status(200).json({
                 success: true,
                 message:"User has been registered successfully"
             })
+        })
+        .catch(err => {
+            console.log(err)
         })
     })
 }
