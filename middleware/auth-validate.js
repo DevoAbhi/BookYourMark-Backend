dc796fd9bd95639086b56423ffe444f31cf2a58a
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-20 23:45:01 +0530

    Add auth validate logic to accept token from frontend

--- middleware/auth-validate.js
+++ middleware/auth-validate.js
@@ -2,13 +2,31 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
-        jwt.verify(token, 'secret_which_I_have_kept_small_but_has_to_be_longer');
-        next();
+        const token = req.get('X-AUTH-TOKEN')
+
+        let user_credentials =jwt.verify(token, 'secret_which_I_have_kept_small_but_has_to_be_longer');
+
+        if(user_credentials){
+            req.token = user_credentials;
+            next();
+        }
+        else{
+            throw new Error('Invalid Token!')
+        }
+        
     }
     catch(error) {
-        res.status(401).json({
-            message: "Auth failed!"
+
+        if(error.name == 'TokenExpiredError') {
+            error.message = 'Token has expired.... Please login again.'
+        }
+        else{
+            console.log(error);
+            error.message = 'Invalid Token.... Please login again.'
+        }
+        return res.status(401).json({
+            success : false,
+            message: error.message
         })
     }
 }
