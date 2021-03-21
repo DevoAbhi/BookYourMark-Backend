4f28e303d355359de4e150104891b73b34aa638c
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-21 23:17:27 +0530

    Register rename folder router

--- routes/folders.js
+++ routes/folders.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const isAuthenticated = require('../middleware/auth-validate');
 const foldersController = require('../controllers/folders')
 
+
+// Post routes
 router.post('/create-folder', isAuthenticated, foldersController.postCreateFolder)
+router.post('/rename-folder', isAuthenticated, foldersController.postRenameFolder)
 
+// Get routes
 router.get('/view-folders', isAuthenticated, foldersController.getFolders)
-
-router.put('/rename-folder', isAuthenticated)
 
 
 
