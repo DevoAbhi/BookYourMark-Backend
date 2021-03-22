f22e1f57765d29e2082d4305fd18977bda9d9e49
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-22 18:08:56 +0530

    Add delete folder route and add delete folder logic in the contoller

--- routes/folders.js
+++ routes/folders.js
@@ -7,6 +7,7 @@ const foldersController = require('../controllers/folders')
 // Post routes
 router.post('/create-folder', isAuthenticated, foldersController.postCreateFolder)
 router.post('/rename-folder', isAuthenticated, foldersController.postRenameFolder)
+router.delete('/delete-folder', isAuthenticated, foldersController.postDeleteFolder)
 
 // Get routes
 router.get('/view-folders', isAuthenticated, foldersController.getFolders)
