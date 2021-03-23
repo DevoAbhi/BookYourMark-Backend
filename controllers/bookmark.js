46f5f5fbd0dc187f617850e93f263a6746c62acc
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-23 06:39:47 +0530

    Register the get route and give logic in the controller

--- controllers/bookmark.js
+++ controllers/bookmark.js
@@ -1,22 +1,28 @@
 const Bookmark = require('../models/booksmarks');
+const User = require('../models/user');
 
-exports.postCreateBookmark = (req, res, next) => {
+exports.postCreateBookmark = async (req, res, next) => {
     const bookmark_title = req.body.bookmark_title;
     const href = req.body.href;
     const description = req.body.description;
     const folder_id = req.body.folder_id;
 
+    const user = await User.findOne({'folders._id': folder_id})
+    const folder = user.folders.find(obj => obj._id == folder_id)
+
     const bookmark = new Bookmark({
         bookmark_title: bookmark_title,
         href: href,
         description: description,
-        folder_id : folder_id
+        folder_id: folder._id,
+        folder_title : folder.folder_title,
     })
 
     return bookmark.save().then(result => {
         console.log(result);
         return res.status(200).json({
             success: true,
+            bookmark: result,
             message: "Bookmark has been added!"
         })
     })
@@ -29,4 +35,35 @@ exports.postCreateBookmark = (req, res, next) => {
     })
 
 
+}
+
+exports.getBookmarks = async (req, res, next) => {
+    
+    try{
+    
+        const folderId = req.query.folder_id;
+
+        const bookmarks = await Bookmark.find({folder_id: folderId});
+
+        if(bookmarks.length >= 0){
+            return res.status(200).json({
+                success : true,
+                bookmarks : bookmarks,
+                message: "Got all the bookmarks!"
+            })
+        }
+
+        res.status(500).json({
+            success: true,
+            message: "Invalid data"
+        })
+
+    }   
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong getting bookmarks"
+        })
+    }
 }
--- routes/bookmark.js
+++ routes/bookmark.js
@@ -5,7 +5,8 @@ const isAuthenticated = require('../middleware/auth-validate');
 const bookmarkController = require('../controllers/bookmark')
 
 // Post Routes
-router.post('/create-bookmark', isAuthenticated, bookmarkController.postCreateBookmark)
+router.post('/create-bookmark', isAuthenticated, bookmarkController.postCreateBookmark);
+router.get('/view-bookmarks', isAuthenticated, bookmarkController.getBookmarks)
 
 
 module.exports = router;
